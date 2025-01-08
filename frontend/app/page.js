3bf0bd5b1c2d8fd23559052de8225a12e9a879e0
code-Harsh247 <justharsh2407@gmail.com>
2025-01-09 00:42:41 +0530

    Initial commit

---
+++ frontend/app/page.js
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <div className="bg-primary-yellow font-custom ">
+      <p className="font-regular">Regular</p>
+      <p className="font-medium">Medium</p>
+      <p className="font-semibold">SemiBold</p>
+      <p className="font-bold">Bold</p>
+    </div>
+  )
+}
+
+export default Home;
