8e54d14743c804e3840b281a3a3e28611999508c
Ajitesh-jam <ajitesh.jam@kgpian.iitkgp.ac.in>
2025-01-10 19:49:54 +0530

    data fetching and integration

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,10 +1,12 @@
 import "./globals.css";
-
+import { LoginProvider } from "@/contexts/loginContext";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <LoginProvider>
         {children}
+        </LoginProvider>
       </body>
     </html>
   );
