dce98bb1afe2051378c7896bf58b2502110c4ccf
code-Harsh247 <justharsh2407@gmail.com>
2025-01-11 20:09:13 +0530

    Added footer

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import "./globals.css";
 import { LoginProvider } from "@/contexts/loginContext";
+import Footer from "@/components/footer/footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <LoginProvider>
         {children}
+        <Footer/>
         </LoginProvider>
       </body>
     </html>
