4723894f32eb626daf57caa03e3b1f358cc31efd
chadsaras <saraswagh3421@gmail.com>
2025-01-18 18:29:03 +0530

    testing

---
+++ app/test/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './test.css'
+import DistributeTicket from '@/components/DistributeTicket/DistributeTicket'
+import Button from '@/components/Button/Button'
+const page = () => {
+  return (
+    <div className='Test'>
+        <Button btnText="Press me" ></Button>
+    </div>
+  )
+}
+
+export default page
--- components/ConfirmBooking/ConfirmBooking.js
+++ components/ConfirmBooking/ConfirmBooking.js
@@ -3,35 +3,40 @@ import React from 'react'
 import './ConfirmBooking.css'
 import Button from '../Button/Button'
 const ConfirmBooking = () => {
-    const handleDistributeNow=()=>{
+    const handleDistributeNow = () => {
         console.log("Distribute Now");
     }
     return (
-    <div className ='BookingContainer'>
+        <div className='Container'>
+            <div className='BookingContainer'>
 
-        
-        <div className='FanscoreDetailContainer'>
-            <div className='AddedToQueue'>
-                <p> YOU HAVE BEEN ADDED TO QUEUE!</p>
-            </div>
-            <div className='Fanscore'>
-                <div className='Content'>
-                    <p>YOUR FANSCORE :</p>
-                    <p>89</p>
+
+                <div className='FanscoreDetailContainer'>
+                    <div className='AddedToQueue'>
+                        <p> YOU HAVE BEEN ADDED TO QUEUE!</p>
+                    </div>
+                    <div className='Fanscore'>
+                        <div className='Content'>
+                            <p>YOUR FANSCORE :</p>
+                            <p>89</p>
+                        </div>
+                        <div className='statement'>
+                            <p>*You have been placed in the queue, based on this fanscore</p>
+                        </div>
+                    </div>
+
                 </div>
-                <div className='statement'>
-                    <p>*You have been placed in the queue, based on this fanscore</p>
+
+                <div className='OK'>
+                    <Button btnText="OK" onClickFunction={handleDistributeNow}></Button>
                 </div>
+
+
             </div>
+            
+                <Button btnText="Press me" onClickFunction={handleDistributeNow}></Button>
             
         </div>
-        
-            <div className='OK'>
-                <Button btnText="OK" onClickFunction={handleDistributeNow}></Button> 
-            </div>
-            
-        
-    </div>
     )
 }
 
